9b5f5d8b631902fadc7e8e13428fa13d6ae9b4d0
Hsiaoyi Hsu <xreamxu@gmail.com>
2023-02-21 12:42:39 +0800

    [!] script converter

--- surge/modules/script-converter/script-converter.js
+++ surge/modules/script-converter/script-converter.js
@@ -101,7 +101,7 @@ var _scriptSonverterDone = (val = {}) => {
 let url
 !(async () => {
   if (!$.isRequest()) throw new Error('不是 request')
-  url = $request.url.replace(/_script-converter-(stash|surge)\.js$/, '')
+  url = $request.url.replace(/_script-converter-(stash|surge|loon|shadowrocket)\.js$/i, '')
   $.log(`🔗 原始文件链接`, url)
   const res = await $.http.get({
     url,
